b359951dcf335b22f5c89839ee3e285954a069fd
Peter Sitarz <ps@statscale.com>
2025-02-20 22:04:27 -0500

    Scripted commit

--- sync.ps1
+++ sync.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 
 Write-Host 'Pushing changes to Git for my app!';
 
-$dir = $env:USERPROFILE + "\source\repos\io.statscale.com\"
+$dir = $env:USERPROFILE + "\source\repos\kb.statscale.com\"
 Set-Location $dir
 
 # & .\gentoc.ps1
